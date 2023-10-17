c340626fa81e16aca52f5e75e46e62d11a0e8611
duanwenlong <WB-dwl906436>
2023-10-17 14:44:47 +0800

    test

---
+++ src/main/java/org/corgi/consumer/sourcedownload/configuration/CloudConfig.java
@@ -0,0 +1,73 @@
+package org.corgi.consumer.sourcedownload.configuration;
+
+import com.amazon.sqs.javamessaging.AmazonSQSMessagingClientWrapper;
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnection;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazon.sqs.javamessaging.SQSSession;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.JMSException;
+import javax.jms.Session;
+
+@Configuration
+public class CloudConfig  {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+
+    @Bean(name = "sqsSession")
+    public Session sqsSession() throws JMSException {
+        SQSConnection sqsConnection = sqsConnection();
+        return sqsConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    }
+
+    @Bean(name = "sqsConnection")
+    public SQSConnection sqsConnection() throws JMSException {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        SQSConnectionFactory connectionFactory = new SQSConnectionFactory(
+                new ProviderConfiguration(),
+                AmazonSQSClientBuilder.standard()
+                        .withRegion(region)
+                        .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+        );
+        SQSConnection connection = connectionFactory.createConnection();
+
+        return connection;
+    }
+
+
+    @Bean(name = "s3Client")
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+
+        return s3Client;
+    }
+}
